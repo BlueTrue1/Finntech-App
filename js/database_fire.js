d52a3b79d802c335f6536b373e27cfa35b48dbe9
vmv1997@hotmail.com <SmartCompare19951997>
2018-09-30 15:49:11 +0200

    firebase functionality

---
+++ js/database_fire.js
@@ -0,0 +1,26 @@
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
+function getCollection(CollectionName) {
+    return database.ref(CollectionName).once('value')
+        .then(function (snapshot) {
+            return snapshot.val()
+        })
+}
+
+function setData(collectionName, setDataObject) {
+    return database.ref(collectionName).update(setDataObject)
+}
+
+function setDataForUser(userId, data) {
+    setData('/users/' + userId, data)
+}
+
+getCollection('/users')
+    .then(function (data) {
+        console.log(data)
+    });
+
+
+setDataForUser(2, {'City': 'hier'});
---
+++ readme.md
@@ -0,0 +1,20 @@
+#Setup
+
+Install dependencies
+```
+npm install
+```
+
+Add your own firebase config __js/firebase.config.js__ with your credentials
+
+It must look like this
+```
+var firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
+};
+```
